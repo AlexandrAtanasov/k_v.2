1bd8183b77b02b0db26e8d7a7167582585e80397
AtanasovAlexandr <mephistosaa@gmail.com>
2020-07-31 23:10:02 +0300

    persons page updated

--- pages/persons/index.js
+++ pages/persons/index.js
@@ -1,5 +1,7 @@
 import { MainLayout } from '../../layouts/MainLayout'
-import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import { Container, Row, Col, Card, Button, Tabs, Tab, Nav, CardDeck } from 'react-bootstrap'
+
+import classes from '../../styles/persons.module.scss'
 
 export default function PersonsPage() {
     return (
@@ -7,49 +9,186 @@ export default function PersonsPage() {
             title='Sales page'
             description='Description for Sales page'
         >
-            <Container>
+            <Tab.Container id="persons_cards" defaultActiveKey="first" >
+                <Row className="justify-content-md-center" >
+                    <Nav variant="pills" className={classes.persons_cards_pills}>
+                        <Col lg='auto'>
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Doctors</Nav.Link>
+                            </Nav.Item>
+                        </Col>
+                        <Col lg='auto'>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Admins</Nav.Link>
+                            </Nav.Item>
+                        </Col>
+                        <Col lg='auto'>
+                    <Nav.Item>
+                        <Nav.Link eventKey="third">Treners</Nav.Link>
+                    </Nav.Item>
+                </Col>
+                    </Nav>
+                </Row>
                 <Row className="justify-content-md-center">
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="/img/carousel/img_1.png" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="/img/carousel/img_2.png" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md="auto">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="/img/carousel/img_3.png" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
+                    <Col>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Card>
+                                    <Card.Header>
+                                        Docs
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <CardDeck>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This card has supporting text below as a natural lead-in to additional
+                                                content.{' '}
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This card has even longer content than the first to
+                                                show that equal height action.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        </CardDeck>
+                                    </Card.Body>
+                                </Card>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second">
+                                <Card>
+                                    <Card.Header>
+                                        Adimns
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <CardDeck>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This card has supporting text below as a natural lead-in to additional
+                                                content.{' '}
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This card has even longer content than the first to
+                                                show that equal height action.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        </CardDeck>
+                                    </Card.Body>
+                                </Card>
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="third">
+                                <Card>
+                                    <Card.Header>
+                                        Treners
+                                    </Card.Header>
+                                    <Card.Body>
+                                        <CardDeck>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This content is a little bit longer.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This card has supporting text below as a natural lead-in to additional
+                                                content.{' '}
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        <Card>
+                                            <Card.Img variant="top" src="holder.js/100px160" />
+                                            <Card.Body>
+                                            <Card.Title>Card title</Card.Title>
+                                            <Card.Text>
+                                                This is a wider card with supporting text below as a natural lead-in to
+                                                additional content. This card has even longer content than the first to
+                                                show that equal height action.
+                                            </Card.Text>
+                                            </Card.Body>
+                                            <Card.Footer>
+                                            <small className="text-muted">Last updated 3 mins ago</small>
+                                            </Card.Footer>
+                                        </Card>
+                                        </CardDeck>
+                                    </Card.Body>
+                                </Card>
+                            </Tab.Pane>
+                        </Tab.Content>
                     </Col>
                 </Row>
-            </Container>
+            </Tab.Container>
         </MainLayout>
     )
 } 
