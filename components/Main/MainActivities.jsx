3bb39c74dfc2552ef6ebc8c6be3c9f2d2c1086ae
AtanasovAlexandr <mephistosaa@gmail.com>
2020-08-03 23:26:15 +0300

    main page activities grid updated

--- components/Main/MainActivities.jsx
+++ components/Main/MainActivities.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import { Container, Row, Col, Card } from 'react-bootstrap'
+import { Container, Row, Col, Card, Image } from 'react-bootstrap'
 
 export function MainActivities() {
 
     return (
         <Container fluid>
-            <Row xs={3} md={5} >
-                <Col><img height='100' src='/img/main/activities/activities (1).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (2).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (3).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (4).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (5).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (6).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (7).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (8).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (9).jpg'/></Col>
-                <Col><img height='100' src='/img/main/activities/activities (10).jpg'/></Col>
+            <Row 
+                className="justify-content-center" 
+                // className="justify-content-md-center" 
+                xs={4} 
+                md={6} 
+                lg={10}
+                xl
+            >
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (1).jpg" fluid /><span>First</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (2).jpg" fluid /><span>Second</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (3).jpg" fluid /><span>Third</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (4).jpg" fluid /><span>Fourth</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (5).jpg" fluid /><span>Fifth</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (6).jpg" fluid /><span>Sixth</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (7).jpg" fluid /><span>Seventh</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (8).jpg" fluid /><span>Eighth</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (9).jpg" fluid /><span>Ninth</span></Col>
+                <Col className="justify-content-center" ><Image style={{maxHeight: "5rem"}} src="/img/main/activities/activities (10).jpg" fluid /><span>Tenth</span></Col>
             </Row>
         </Container>
-    )
+)
 }
