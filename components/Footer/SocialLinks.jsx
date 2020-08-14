24562f817868dffd22ac566536894501287f42a2
AtanasovAlexandr <mephistosaa@gmail.com>
2020-08-14 17:21:30 +0300

    basic component for social at footer

---
+++ components/Footer/SocialLinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function SocialLinks() {
+    return (
+        <>
+            <p>Our social networks:</p>
+            <div>Instagram</div>
+            <div>VK</div>
+            <div>Facebook</div>
+        </>
+    )
+}
--- components/FooterComponent.jsx
+++ components/FooterComponent.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 import { Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import { SocialLinks } from './Footer/SocialLinks'
 
 export function FooterComponent() {
     return (
         <footer className='footer mt-auto py-3 bg-dark text-white'>
             <div className='container'>Place sticky footer content here.</div>
+            <SocialLinks />
         </footer>
     )
 }
