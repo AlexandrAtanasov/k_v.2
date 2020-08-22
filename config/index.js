f77d7d3c0629c8b2d0fd77c0c69ce1795d3a5377
AtanasovAlexandr <mephistosaa@gmail.com>
2020-08-22 23:49:42 +0300

    dynamic prod server

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 // export const server = dev ? `http://localhost:3000` : `https://vercel.com/alexandratanasov/k-v-2/`
 // export const server = dev ? `http://localhost:3000` : `https://k-v-2.vercel.app/`
-export const server = dev ? `http://localhost:3000` : `https://k-v-2-hsf67ojqb.vercel.app`
+export const server = dev ? `http://localhost:3000` : document.location.origin
