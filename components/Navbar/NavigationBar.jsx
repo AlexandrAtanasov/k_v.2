62a064ef11dae39e0506d609b83a5eb20641a99c
AtanasovAlexandr <mephistosaa@gmail.com>
2020-09-01 23:49:03 +0300

    close dropdown by click outside only then has class show

--- components/Navbar/NavigationBar.jsx
+++ components/Navbar/NavigationBar.jsx
@@ -57,7 +57,28 @@ export function NavigationBar() {
             return;
         }
         // outside click
-        document.getElementById("basic-navbar-nav-button").click();
+        
+        if (document.getElementById("basic-navbar-nav").classList.contains('show')) {
+            document.getElementById("basic-navbar-nav-button").click();
+        } 
+
+
+        // const browser= (function(){
+        //     var ua= navigator.userAgent, tem,
+        //     M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+        //     if(/trident/i.test(M[1])){
+        //         tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        //         return 'IE '+(tem[1] || '');
+        //     }
+        //     if(M[1]=== 'Chrome'){
+        //         tem= ua.match(/\b(OPR|Edge?)\/(\d+)/);
+        //         if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera').replace('Edg ', 'Edge ');            
+        //     }
+        //     M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+        //     if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+        //     // return M.join(' ');
+        //     return M;
+        // })();
     };
     
     useEffect(() => {
