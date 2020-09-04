4b62130c1014516d9e117f09344a57b62ed52e86
AtanasovAlexandr <mephistosaa@gmail.com>
2020-09-04 23:46:35 +0300

    call buttons added to locations page

--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,7 +1,14 @@
 import { Card, Button } from 'react-bootstrap'
-
 import { MainLayout } from '../../layouts/MainLayout'
+
+// import Components
+import { ModalCallback } from '../../components/Modal_Callback/Modal_Callback'
+import { ModalContacts } from '../../components/Modal_Contacts/Modal_Contacts'
+
 import { maps } from '../../data/maps/location_of_centers'
+
+import style from '../../styles/contacts_page.module.scss'
+
 
 export default function LocationsPage() {
     return (
@@ -24,6 +31,19 @@ export default function LocationsPage() {
                             </Card.Text>
                             <script type="text/javascript" charSet="utf-8" async src={`${elem.map}`}></script>
                         </Card.Body>
+
+                        <div className={` ${style.div_for_button} `} >
+                            <Button variant="success">
+                                <ModalCallback />
+                            </Button>
+
+                        </div>
+                        <div className={` ${style.div_for_button} `}>
+                            <Button variant="success">
+                                <ModalContacts />
+                            </Button>
+
+                        </div>
                     </Card>
                 )
             })}
