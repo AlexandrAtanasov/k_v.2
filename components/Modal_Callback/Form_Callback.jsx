39c6dd40de0906450a6ea81c94a4434bc637aba8
AtanasovAlexandr <mephistosaa@gmail.com>
2020-09-16 23:13:07 +0300

    email sending added. step 1

--- components/Modal_Callback/Form_Callback.jsx
+++ components/Modal_Callback/Form_Callback.jsx
@@ -1,226 +1,19 @@
-//  import { Formik } from 'formik'
-//  import { Form, Button, Col, Row, InputGroup } from 'react-bootstrap'
-//  import * as yup from 'yup'
-
-// const schema = yup.object({
-//   userName: yup.string().required(),
-//   lastName: yup.string().required(),
-//   username: yup.string().required(),
-//   city: yup.string().required(),
-//   state: yup.string().required(),
-//   zip: yup.string().required(),
-//   terms: yup.bool().required(),
-// });
-
-// export function OrderCallForm() {
-//   return (
-//     <Formik
-//       validationSchema={schema}
-//       onSubmit={alert('asdasd')}
-      
-//       initialValues={{
-//         userName: '',
-//         lastName: '',
-//       }}
-//     >
-//       {({
-//         handleSubmit,
-//         handleChange,
-//         handleBlur,
-//         values,
-//         touched,
-//         isValid,
-//         errors,
-//       }) => (
-//         <Form noValidate onSubmit={handleSubmit}>
-//           <Form.Row>
-//             <Form.Group  controlId="validationFormik01">
-//               <Form.Label>User name</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 name="userName"
-//                 value={values.userName}
-//                 onChange={handleChange}
-//                 isValid={touched.userName && !errors.userName}
-//               />
-//               <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-//             </Form.Group>
-           
-            
-            
-//           </Form.Row>
-
-//           <Form.Row>
-//             <Form.Group as={Col} md="6" controlId="validationFormik03">
-//               <Form.Label>City</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="City"
-//                 name="city"
-//                 value={values.city}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.city}
-//               />
-
-//               <Form.Control.Feedback type="invalid">
-//                 {errors.city}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group as={Col} md="3" controlId="validationFormik04">
-//               <Form.Label>State</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="State"
-//                 name="state"
-//                 value={values.state}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.state}
-//               />
-//               <Form.Control.Feedback type="invalid">
-//                 {errors.state}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//             <Form.Group as={Col} md="3" controlId="validationFormik05">
-//               <Form.Label>Zip</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 placeholder="Zip"
-//                 name="zip"
-//                 value={values.zip}
-//                 onChange={handleChange}
-//                 isInvalid={!!errors.zip}
-//               />
-
-//               <Form.Control.Feedback type="invalid">
-//                 {errors.zip}
-//               </Form.Control.Feedback>
-//             </Form.Group>
-//           </Form.Row>
-
-//         <Form.Group controlId="formBasicChoose">
-//             <Form.Label>Select Location</Form.Label>
-//             <Form.Control 
-//                 as="select" 
-//                 defaultValue="Choose location..."
-            
-//                 type="text"
-//                 name="address"
-//                 value={values.address}
-//                 onChange={handleChange}
-//             >
-//                 <option>Location #1...</option>
-//                 <option>Location #2...</option>
-//                 <option>Location #3...</option>
-//             </Form.Control>
-//         </Form.Group>
-
-          
-//           <Button type="submit">Submit form</Button>
-//         </Form>
-//       )}
-//     </Formik>
-//   );
-// }
-
-
-//  0
-// import React from 'react'
-// import { Form, Button } from 'react-bootstrap'
-// import { Formik } from 'formik'
-
-// export function OrderCallForm() {
-
-//     const handleSubmit = () => console.log('submit');
-// return (
-//     <Formik
-//       onSubmit={console.log}
-      
-      
-//       initialValues={{
-//         userName: '',
-//         userNumber: '',
-//         userSelectedLocation: ''
-//       }}
-//     >
-//         {({
-//             handleSubmit,
-//             handleChange,
-//             handleBlur,
-//             values,
-//             touched,
-//             isValid,
-//             errors,
-//         }) => (
-//             <Form>
-//                 <Form.Group controlId="formBasicName">
-//                     <Form.Label>Your name</Form.Label>
-//                     <Form.Control 
-//                         placeholder="Enter here your name" 
-
-//                         type="text"
-//                         name="userName"
-//                         value={values.userName}
-//                         onChange={handleChange}
-//                     />
-//                     <Form.Text className="text-muted">
-//                         Some text here...
-//                     </Form.Text>
-//                 </Form.Group>
-
-//                 <Form.Group controlId="formBasicPassword">
-//                     <Form.Label>Your phone number</Form.Label>
-//                     <Form.Control 
-//                         type='tel' 
-//                         placeholder="Your phone number" 
-                    
-                        
-//                         name="userNumber"
-//                         value={values.userNumber}
-//                         onChange={handleChange}
-//                     />
-//                     <Form.Text className="text-muted">
-//                         Phone number like - +79999999999
-//                     </Form.Text>
-//                 </Form.Group>
-
-//                 <Form.Group controlId="formBasicChoose">
-//                     <Form.Label>Select Location</Form.Label>
-//                     <Form.Control 
-//                         as="select" 
-//                         // defaultValue="Choose location..."
-
-//                         type="text"
-//                         name="userSelectedLocation"
-//                         value={values.userSelectedLocation}
-//                         onChange={handleChange}
-//                     >
-//                         <option>Location #1...</option>
-//                         <option>Location #2...</option>
-//                         <option>Location #3...</option>
-//                     </Form.Control>
-//                 </Form.Group>
-
-//                 <Button variant="success" type="submit" onClick={handleSubmit}>
-//                     Submit
-//                 </Button>
-//             </Form>
-//         )}
-//     </Formik>
-//   );
-//         }
-
-
 import React from 'react';
 import { useFormik } from 'formik';
 import { Form, Button } from 'react-bootstrap'
 
 import { server } from '../../config'
 
-// import{ init } from 'emailjs-com';
-// init("user_CC844BqNZGwSxOC7pxkNc");
+import emailjs from 'emailjs-com';
 
 
 const OrderCallForm = () => {
+
+     
+    const service = process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID;
+    const template = process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID;
+    const user = process.env.NEXT_PUBLIC_EMAIL_USER_ID;
+
   // Notice that we have to initialize ALL of fields with values. These
   // could come from props, but since we don't want to prefill this form,
   // we just use an empty string. If you don't do this, React will yell
@@ -229,11 +22,17 @@ const OrderCallForm = () => {
     initialValues: {
       userName: '',
       userNumber: '',
+      userLocation: '',
     },
     onSubmit: values => {
-        // console.log(JSON.stringify(values, null, 2))
-        alert(JSON.stringify(values, null, 2))
-
+        console.log(JSON.stringify(values, null, 2))
+        emailjs.send(service, template, values, user)
+        .then(function(response) {
+           console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+           console.log('FAILED...', error);
+        });
+        // go to main after submit
         // window.location.href = "/";
     },
   });
@@ -246,8 +45,6 @@ const OrderCallForm = () => {
                     type="text"
                     // id="userName"
                     name="userName"
-                    // value={values.userName}
-                //  onChange={handleChange}
                     onChange={formik.handleChange}
                     value={formik.values.firstName}
                 />
@@ -261,7 +58,6 @@ const OrderCallForm = () => {
                 <Form.Control 
                     type='tel' 
                     // placeholder="Your phone number" 
-            
                     // id="userNumber"
                     name="userNumber"
                     onChange={formik.handleChange}
@@ -272,6 +68,25 @@ const OrderCallForm = () => {
                 </Form.Text>
             </Form.Group>
 
+
+            <Form.Group controlId="formBasicChoose">
+                <Form.Label>Select Location</Form.Label>
+                <Form.Control 
+                    as="select" 
+                    // defaultValue="Choose location..."
+                    // type='text' 
+                    // placeholder="Your phone number" 
+                    // id="userNumber"
+                    name="userLocation"
+                    onChange={formik.handleChange}
+                    value={formik.values.userLocation}
+                >
+                    <option>Location #1...</option>
+                    <option>Location #2...</option>
+                    <option>Location #3...</option>
+                </Form.Control>
+            </Form.Group>
+
             <Button variant="success" type="submit">
                 Submit
             </Button>
@@ -280,153 +95,3 @@ const OrderCallForm = () => {
 };
 
 export default OrderCallForm;
-
-
-
-//     return (
-//         <Form>
-//             <Form.Group controlId="formBasicName">
-//                 <Form.Label>Your name</Form.Label>
-//                 <Form.Control type='text' placeholder="Enter here your name" />
-//                 <Form.Text className="text-muted">
-//                     Some text here...
-//                 </Form.Text>
-//             </Form.Group>
-
-//             <Form.Group controlId="formBasicPassword">
-//                 <Form.Label>Your phone number</Form.Label>
-//                 <Form.Control type='tel' placeholder="Your phone number" />
-//                 <Form.Text className="text-muted">
-//                     Phone number like - +79999999999
-//                 </Form.Text>
-//             </Form.Group>
-
-//             <Form.Group controlId="formBasicChoose">
-//                 <Form.Label>Select Location</Form.Label>
-//                 <Form.Control as="select" defaultValue="Choose location...">
-//                     <option>Location #1...</option>
-//                     <option>Location #2...</option>
-//                     <option>Location #3...</option>
-//                 </Form.Control>
-//             </Form.Group>
-
-//             <Button variant="success" type="submit" onClick={handleSubmit}>
-//                 Submit
-//             </Button>
-//         </Form>
-//     )
-// }
-
-
-// 2
-// import React from 'react';
-//  import { Formik } from 'formik';
- 
-//  const OrderCallForm = () => (
-//    <div>
-//      <h1>Anywhere in your app!</h1>
-//      <Formik
-//        initialValues={{ email: '', password: '' }}
-//        validate={values => {
-//          const errors = {};
-//          if (!values.email) {
-//            errors.email = 'Required';
-//          } else if (
-//            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//          ) {
-//            errors.email = 'Invalid email address';
-//          }
-//          return errors;
-//        }}
-//        onSubmit={(values, { setSubmitting }) => {
-//          setTimeout(() => {
-//            alert(JSON.stringify(values, null, 2));
-//            setSubmitting(false);
-//          }, 400);
-//        }}
-//      >
-//        {({
-//          values,
-//          errors,
-//          touched,
-//          handleChange,
-//          handleBlur,
-//          handleSubmit,
-//          isSubmitting,
-//          /* and other goodies */
-//        }) => (
-//          <form onSubmit={handleSubmit}>
-//            <input
-//              type="email"
-//              name="email"
-//              onChange={handleChange}
-//              onBlur={handleBlur}
-//              value={values.email}
-//            />
-//            {errors.email && touched.email && errors.email}
-//            <input
-//              type="password"
-//              name="password"
-//              onChange={handleChange}
-//              onBlur={handleBlur}
-//              value={values.password}
-//            />
-//            {errors.password && touched.password && errors.password}
-//            <button type="submit" disabled={isSubmitting}>
-//              Submit
-//            </button>
-//          </form>
-//        )}
-//      </Formik>
-//    </div>
-//  );
- 
-//  export default OrderCallForm;
-
-
-
-
-
-
-
-//  1
-// import React from 'react'
-// import { Form, Button } from 'react-bootstrap'
-
-// export function OrderCallForm() {
-
-//     const handleSubmit = () => console.log('submit');
-
-//     return (
-//         <Form>
-//             <Form.Group controlId="formBasicName">
-//                 <Form.Label>Your name</Form.Label>
-//                 <Form.Control type='text' placeholder="Enter here your name" />
-//                 <Form.Text className="text-muted">
-//                     Some text here...
-//                 </Form.Text>
-//             </Form.Group>
-
-//             <Form.Group controlId="formBasicPassword">
-//                 <Form.Label>Your phone number</Form.Label>
-//                 <Form.Control type='tel' placeholder="Your phone number" />
-//                 <Form.Text className="text-muted">
-//                     Phone number like - +79999999999
-//                 </Form.Text>
-//             </Form.Group>
-
-//             <Form.Group controlId="formBasicChoose">
-//                 <Form.Label>Select Location</Form.Label>
-//                 <Form.Control as="select" defaultValue="Choose location...">
-//                     <option>Location #1...</option>
-//                     <option>Location #2...</option>
-//                     <option>Location #3...</option>
-//                 </Form.Control>
-//             </Form.Group>
-
-//             <Button variant="success" type="submit" onClick={handleSubmit}>
-//                 Submit
-//             </Button>
-//         </Form>
-//     )
-// }
--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,12 @@ class MyDocument extends Document {
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
             <script src="//code.jivosite.com/widget.js" data-jv-id="j5UxZwUNyF" async></script>
 
-            <script type="text/javascript"src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
+            {/* <script type="text/javascript"src="https://cdn.jsdelivr.net/npm/emailjs-com@2/dist/email.min.js"></script>
             <script type="text/javascript">
               (function() {
                 emailjs.init("user_CC844BqNZGwSxOC7pxkNc")
               })();
-            </script>
+            </script> */}
 
         </Head>
         <body className="h-100">
