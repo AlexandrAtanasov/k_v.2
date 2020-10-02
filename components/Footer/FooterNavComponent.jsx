a0acb6c80b0a428e504c20e4de5e3187bf069d28
AtanasovAlexandr <mephistosaa@gmail.com>
2020-10-02 23:33:16 +0300

    added localization file for footer nav component

--- components/Footer/FooterNavComponent.jsx
+++ components/Footer/FooterNavComponent.jsx
@@ -5,20 +5,24 @@ import { Nav } from 'react-bootstrap'
 // import style
 import style from '../../styles/components/footer/FooterNavComponent.module.scss'
 
+
+// import localization data
+import { local } from '../../data/localization_data/components/Footer/FooterNavComponent'
+
 export function FooterNavComponent() {
     return (
         <Nav 
             // className="justify-content-md-center"
             className={` ${style.footer_nav_component_display} ${style.footer_nav_component_margin} `}    
         >
-            <Link href={'#sitemap'} passHref>
+            <Link href={` ${local.link_first.link} `} passHref>
                 <Nav.Link>
-                    Sitemap
+                    {local.link_first.text}
                 </Nav.Link>
             </Link>
-            <Link href={'#other_info'} passHref>
+            <Link href={` ${local.link_second.link} `} passHref>
                 <Nav.Link>
-                    Other information
+                    {local.link_second.text}
                 </Nav.Link>
             </Link>
         </Nav>
---
+++ data/localization_data/components/Footer/FooterNavComponent.js
@@ -0,0 +1,10 @@
+export const local = {
+    link_first: {
+        link: "/sitemap",
+        text: "Sitemap"
+    },
+    link_second: {
+        link: "/other_info",
+        text: "Other information"
+    },
+}
