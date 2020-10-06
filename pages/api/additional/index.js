a31f81e6477a211d89f2271d0b6e909253492e47
AtanasovAlexandr <mephistosaa@gmail.com>
2020-10-06 23:33:36 +0300

    updated api for no reason

--- pages/api/additional/index.js
+++ pages/api/additional/index.js
@@ -1,4 +1,4 @@
-import { additional_pages } from '../../../data/additional_pages'
+import { additional_pages } from '../../../data/pages/additional_pages'
 
 export default function handler(req, res) {
   res.status(200).json(additional_pages)
--- pages/api/resolvable/index.js
+++ pages/api/resolvable/index.js
@@ -1,4 +1,4 @@
-import { resolvable_pages } from '../../../data/resolvable_pages'
+import { resolvable_pages } from '../../../data/pages/resolvable_pages'
 
 export default function handler(req, res) {
   res.status(200).json(resolvable_pages)
