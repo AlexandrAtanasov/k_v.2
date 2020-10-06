353af3107bd93ba4149d49205afc6deef584cea4
AtanasovAlexandr <mephistosaa@gmail.com>
2020-10-06 23:39:16 +0300

    api deleted

--- pages/api/additional/index.js
+++
@@ -1,5 +0,0 @@
-import { additional_pages } from '../../../data/pages/additional_pages'
-
-export default function handler(req, res) {
-  res.status(200).json(additional_pages)
-}
--- pages/api/resolvable/index.js
+++
@@ -1,5 +0,0 @@
-import { resolvable_pages } from '../../../data/pages/resolvable_pages'
-
-export default function handler(req, res) {
-  res.status(200).json(resolvable_pages)
-}
