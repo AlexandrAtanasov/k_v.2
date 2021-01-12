c899153ad4e3176851828ea4bb2e0e2cd836bee2
AtanasovAlexandr <mephistosaa@gmail.com>
2021-01-12 21:44:13 +0300

    change menu text (resolvable)

--- data/localization_data/components/Main/Navbar/Navbar.js
+++ data/localization_data/components/Main/Navbar/Navbar.js
@@ -1,15 +1,15 @@
 export const local = {
       menu_additional: 'Дополнительные услуги',
-      menu_resolvable: 'Решаемые проблемы',
+      menu_resolvable: 'Что мы лечим',
       menu_contacts: 'Контакты',
       menu_news: 'Новости',
       menu_how_to_find_us: 'Как нас найти',
       form_number_info: 'Phone number like - +7 (999) 999-99-99',
-      form_location_label: 'Select Location',
+      form_location_label: 'Выберите адрес центра',
       form_location_options: {
-          option_1: "Location #1...",
-          option_2: "Location #2...",
-          option_3: "Location #3...",
+          option_1: "Крупской 31",
+          option_2: "Ленина 87",
+          option_3: "Маршала Рыбалко 85В",
       },
-      form_button: "Submit"
+      form_button: "Отправить"
 }
