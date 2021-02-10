dbd9a228ce18186f59794513b066684062d5b375
AtanasovAlexandr <mephistosaa@gmail.com>
2021-02-10 22:17:46 +0300

    minor fix - deleted import

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-import Link from 'next/link'
 import { MainLayout } from '../layouts/MainLayout'
 import { CarouselComponent } from '../components/Main/CarouselComponent'
 import { MainActivitiesComponent } from '../components/Main/MainActivitiesComponent'
 import { MainAdvantages } from '../components/Main/MainAdvantages'
 import { MainDoctorsComponent } from '../components/Main/MainDoctorsComponent'
 import { MainLicensesComponent } from '../components/Main/MainLicensesComponent'
-import { CardComponentImg } from '../components/CardComponents/CardComponentImg'
 import { HeadingComponent } from '../components/Heading/HeadingComponent'
 import { MainCentersComponent } from '../components/Main/MainCentersComponent'
 import { MainStartIntro } from '../components/Main/MainStartIntro'
 
 // import data
 import { carousel } from '../data/main/carousel/main_carousel'
-// import { authors_column } from '../data/pages/authors_column/authors_column'
 
 export default function HomePage() {
   
