78631f2082a63231837a7f71ff095ff05b64cc32
AtanasovAlexandr <mephistosaa@gmail.com>
2021-02-25 22:20:18 +0300

    facebook pixel added

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,24 @@ import NextNprogress from 'nextjs-progressbar';
 import '../styles/main.scss'
 import '../styles/global_colors.scss'
 
+// tags
+import { YMInitializer } from 'react-yandex-metrika';
+import ReactPixel from 'react-facebook-pixel';
+import ReactGA from 'react-ga';
 
 
 export default function MyApp({ Component, pageProps }) {
+    
+    // google analytics
+    ReactGA.initialize('263380978');
+    // facebook pixel
+    ReactPixel.init('2609870085929044', advancedMatching, options);
+    ReactPixel.pageView();
+    
     return (
         <>
+            <YMInitializer accounts={[54542998]} />
+
             <NextNprogress
                 // color="#29D"
                 // startPosition="1"
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-// tags
-import { YMInitializer } from 'react-yandex-metrika';
-import ReactGA from 'react-ga';
-
 class MyDocument extends Document {
   
   render() {
-
-    ReactGA.initialize('263380978');
 
     return (
       <Html className="h-100">
@@ -16,8 +10,6 @@ class MyDocument extends Document {
             <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous" />
             <script src="//code.jivosite.com/widget.js" data-jv-id="j5UxZwUNyF" async></script>
-
-            <YMInitializer accounts={[54542998]} />
 
         </Head>
         <body className="h-100">
