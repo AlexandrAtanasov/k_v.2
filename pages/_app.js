067d6e1871c3b09d7a1c6d0359693cd53d8ae971
AtanasovAlexandr <mephistosaa@gmail.com>
2021-03-01 23:11:39 +0300

    next-ym added

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NextNprogress from 'nextjs-progressbar';
 
+import Router from "next/router";
+import withYM from "next-ym";
+
 // import styles
 import '../styles/main.scss'
 import '../styles/global_colors.scss'
 
 // tags
-import { YMInitializer } from 'react-yandex-metrika';
+// import { YMInitializer } from 'react-yandex-metrika';
 
-export default function MyApp({ Component, pageProps }) {
+export default withYM("54928126", Router)(MyApp);
+
+function MyApp({ Component, pageProps }) {
     
     return (
         <>
-            <YMInitializer accounts={[]} options={{accurateTrackBounce: true}, {trackLinks: true}, {}} />
+            {/* <YMInitializer accounts={[54928126]} options={{accurateTrackBounce: true}, {trackLinks: true}} /> */}
             <NextNprogress/>
             <Component {...pageProps} />
         </>
