8509b2e075b4cd9fbd089cfb194c7c0c7766ab9d
AtanasovAlexandr <mephistosaa@gmail.com>
2021-09-01 22:26:08 +0300

    updated: цены на тейпирование

--- data/pages/price/price_kinesitaping/price_kinesitaping.js
+++ data/pages/price/price_kinesitaping/price_kinesitaping.js
@@ -1,7 +1,7 @@
 export const price_kinesitaping = [
     {
       "duration": "1 зона (50 см)", 
-      "cost": "550" ,
-      "cost_club": "500" 
+      "cost": "600" ,
+      "cost_club": "540" 
     }
 ]
